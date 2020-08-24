31f1034aed4668e1b1101921d9d769fad8936f0e
Alex Stocks <stocksalex@icloud.com>
2020-08-24 12:09:36 +0100

    Implemented BHPFuzzer

--- BurpFuzzer.py
+++ BurpFuzzer.py
@@ -18,5 +18,40 @@ class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
     def createNewInstance(self, attack):
         return BHPFuzzer(self, attack)
 
+# extends IIntruderPayloadGenerator which declares 3 methods...
+# hasMorePayloads - checks if reached payload limit
+# getNextPayload - receives payload from http request and fuzzes it
+# reset - resets the factory
+class BHPFuzzer(IIntruderPayloadGenerator):
+    def __init__(self, extender, attack):
+        self._extender = extender
+        self._helpers = extender._helpers
+        self._attack = attack
+        self.max_payloads = 10
+        self.num_iterations = 0
+
+        return
+
+    def hasMorePayloads(self):
+        if self.num_iterations == self.max_payloads:
+            return False
+        else:
+            return True
+
+    def getNextPayload(self, current_payload):
+
+        payload = "".join(chr(x) for x in current_payload) # convert byte array to string
+
+        payload = self.mutate_payload(payload)
+
+        self.num_iterations += 1
+
+        return payload
+
+    def reset(self):
+        self.num_iterations = 0
+        return
+
+
 
 
